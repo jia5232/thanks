25a1fea2515734552bce5882086a065611251479
jia5232 <jia5232@naver.com>
2023-07-31 17:34:40 +0900

    feat: add and apply getit package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:thanks_life_daily/database/drift_database.dart';
 import 'package:thanks_life_daily/screen/home_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -8,6 +9,8 @@ void main() async {
   await initializeDateFormatting();
 
   final database = LocalDatabase();
+
+  GetIt.I.registerSingleton<LocalDatabase>(database);
 
   runApp(
     MaterialApp(
